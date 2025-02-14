5d73befa517c612bb288f855f2e0cc428873c82d
Hapseleg <nicolajljo@gmail.com>
2025-02-14 17:26:44 +0100

    init

--- nodes.py
+++ nodes.py
@@ -1,14 +1,11 @@
 #  Package Modules
 import os
-from typing import Union, BinaryIO, Dict, List, Tuple, Optional
-import time
+import csv
+
 
 #  ComfyUI Modules
 import folder_paths
 from comfy.utils import ProgressBar
-
-#  Your Modules
-from .modules.calculator import CalculatorModel
 
 
 #  Basic practice to get paths from ComfyUI
@@ -20,133 +17,42 @@ custom_nodes_output_dir = os.path.join(folder_paths.get_output_directory(), "my-
 #  These are example nodes that only contains basic functionalities with some comments.
 #  If you need detailed explanation, please refer to : https://docs.comfy.org/essentials/custom_node_walkthrough
 #  First Node:
-class MyModelLoader:
-    #  Define the input parameters of the node here.
+
+def read_ratio_presets():
+    p = os.path.dirname(os.path.realpath(__file__))
+    file_path = os.path.join(p, 'preset_ratios.csv')
+    preset_ratios_dict = {}
+    labels = []
+    with open(file_path, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter='|', quotechar='"')
+        for row in reader:
+            preset_ratios_dict[row[0]] = [row[1],row[2]]
+            labels.append(row[0])
+    return preset_ratios_dict, labels
+
+class SimpleRatioSelector:
     @classmethod
     def INPUT_TYPES(s):
-        my_models = ["Model A", "Model B", "Model C"]
+        s.ratio_presets = read_ratio_presets()[1]
+        s.preset_ratios_dict = read_ratio_presets()[0]
+        return {"required": { "select_preset": (s.ratio_presets, {"default": s.ratio_presets[0]}),
+                              "orientation": (['Portrait','Landscape'], {"default": 'Portrait'})
+                            },
+                "hidden": {"unique_id": "UNIQUE_ID", "extra_pnginfo": "EXTRA_PNGINFO", "prompt": "PROMPT"}}
 
-        return {
-            #  If the key is "required", the value must be filled.
-            "required": {
-                #  `my_models` is the list, so it will be shown as a dropdown menu in the node. ( So that user can select one of them. )
-                #  You must provide the value in the tuple format. e.g. ("value",) or (3,) or ([1, 2],) etc.
-                "model": (my_models,),
-                "device": (['cuda', 'cpu', 'auto'],),
-            },
-            #  If the key is "optional", the value is optional.
-            "optional": {
-                "compute_type": (['float32', 'float16'],),
-            }
-        }
+    RETURN_TYPES = ('INT', 'INT')
+    RETURN_NAMES = ('width', 'height')
+    CATEGORY = 'utils'
+    FUNCTION = 'run'
 
-    #  Define these constants inside the node.
-    #  `RETURN_TYPES` is important, as it limits the parameter types that can be passed to the next node, in `INPUT_TYPES()` above.
-    RETURN_TYPES = ("MY_MODEL",)
-    RETURN_NAMES = ("my_model",)
-    #  `FUNCTION` is the function name that will be called in the node.
-    FUNCTION = "load_model"
-    #  `CATEGORY` is the category name that will be used when user searches the node.
-    CATEGORY = "CustomNodesTemplate"
+    def run(self, select_preset, orientation, unique_id=None, extra_pnginfo=None, prompt=None):
+        dimensions = self.preset_ratios_dict[select_preset]
 
-    #  In the function, use same parameter names as you specified in `INPUT_TYPES()`
-    def load_model(self,
-                   model: str,
-                   device: str,
-                   compute_type: Optional[str] = None,
-                   ) -> Tuple[CalculatorModel]:
-        calculator_model = CalculatorModel()
-        calculator_model.load_model(model, device, compute_type)
+        height = dimensions[0]
+        width = dimensions[1]
 
-        #  You can use `comfy.utils.ProgressBar` to show the progress of the process.
-        #  First, initialize the total amount of the process.
-        total_steps = 5
-        comfy_pbar = ProgressBar(total_steps)
-        #  Then, update the progress.
-        for i in range(1, total_steps):
-            time.sleep(1)
-            comfy_pbar.update(i)  #  Alternatively, you can use `comfy_pbar.update_absolute(value)` to update the progress with absolute value.
+        if orientation == "Landscape":
+            height = dimensions[1]
+            width = dimensions[0]
 
-        #  Return the model as a tuple.
-        return (calculator_model, )
-
-
-#  Second Node
-class CalculatePlus:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "model": ("MY_MODEL", ),
-            },
-            #  Specify the parameters with type and default value.
-            "optional": {
-                "a": ("INT", {"default": 5}),
-                "b": ("INT", {"default": 10}),
-            }
-        }
-
-    RETURN_TYPES = ("INT",)
-    RETURN_NAMES = ("plus_value",)
-    FUNCTION = "plus"
-    CATEGORY = "CustomNodesTemplate"
-
-    def plus(self,
-             model: CalculatorModel,
-             a: Optional[int],
-             b: Optional[int],
-             ) -> Tuple[int]:
-        result = model.plus(a, b)
-        return (result, )
-
-
-
-#  Third Node
-class CalculateMinus:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "model": ("MY_MODEL", ),
-                "a": ("INT", ),
-            },
-            "optional": {
-                "b": ("INT", {"default": 10}),
-            }
-        }
-
-    RETURN_TYPES = ("INT",)
-    RETURN_NAMES = ("minus_value",)
-    FUNCTION = "minus"
-    CATEGORY = "CustomNodesTemplate"
-
-    def minus(self,
-             model: CalculatorModel,
-             a: Optional[int],
-             b: Optional[int],
-             ) -> Tuple[int]:
-        result = model.minus(a, b)
-        return (result, )
-
-
-
-#  Output Node
-class ExampleOutputNode:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "value": ("INT", ),
-            },
-        }
-
-    #  If the node is output node, set this to True.
-    OUTPUT_NODE = True
-    RETURN_TYPES = ("INT",)
-    RETURN_NAMES = ("int",)
-    FUNCTION = "result"
-    CATEGORY = "CustomNodesTemplate"
-
-    def result(self,
-               value: int,) -> Tuple[int]:
-        return (value, )
+        return (width, height)
