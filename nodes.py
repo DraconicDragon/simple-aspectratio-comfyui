a44a12a9e0eb3db86fa91072e03c47e5782d7d37
Drac <altdragon@outlook.de>
2025-02-27 23:49:45 +0100

    Refactor nodes.py with black formatter

--- nodes.py
+++ nodes.py
@@ -1,12 +1,10 @@
 #  Package Modules
-import os
 import csv
-
+import os
 
 #  ComfyUI Modules
 import folder_paths
 from comfy.utils import ProgressBar
-
 
 #  Basic practice to get paths from ComfyUI
 custom_nodes_script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -18,32 +16,37 @@ custom_nodes_output_dir = os.path.join(folder_paths.get_output_directory(), "my-
 #  If you need detailed explanation, please refer to : https://docs.comfy.org/essentials/custom_node_walkthrough
 #  First Node:
 
+
 def read_ratio_presets():
     p = os.path.dirname(os.path.realpath(__file__))
-    file_path = os.path.join(p, 'preset_ratios.csv')
+    file_path = os.path.join(p, "preset_ratios.csv")
     preset_ratios_dict = {}
     labels = []
-    with open(file_path, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter='|', quotechar='"')
+    with open(file_path, newline="") as csvfile:
+        reader = csv.reader(csvfile, delimiter="|", quotechar='"')
         for row in reader:
-            preset_ratios_dict[row[0]] = [row[1],row[2]]
+            preset_ratios_dict[row[0]] = [row[1], row[2]]
             labels.append(row[0])
     return preset_ratios_dict, labels
+
 
 class SimpleRatioSelector:
     @classmethod
     def INPUT_TYPES(s):
         s.ratio_presets = read_ratio_presets()[1]
         s.preset_ratios_dict = read_ratio_presets()[0]
-        return {"required": { "select_preset": (s.ratio_presets, {"default": s.ratio_presets[0]}),
-                              "orientation": (['Portrait','Landscape'], {"default": 'Portrait'})
-                            },
-                "hidden": {"unique_id": "UNIQUE_ID", "extra_pnginfo": "EXTRA_PNGINFO", "prompt": "PROMPT"}}
+        return {
+            "required": {
+                "select_preset": (s.ratio_presets, {"default": s.ratio_presets[0]}),
+                "orientation": (["Portrait", "Landscape"], {"default": "Portrait"}),
+            },
+            "hidden": {"unique_id": "UNIQUE_ID", "extra_pnginfo": "EXTRA_PNGINFO", "prompt": "PROMPT"},
+        }
 
-    RETURN_TYPES = ('INT', 'INT')
-    RETURN_NAMES = ('width', 'height')
-    CATEGORY = 'utils'
-    FUNCTION = 'run'
+    RETURN_TYPES = ("INT", "INT")
+    RETURN_NAMES = ("width", "height")
+    CATEGORY = "utils"
+    FUNCTION = "run"
 
     def run(self, select_preset, orientation, unique_id=None, extra_pnginfo=None, prompt=None):
         dimensions = self.preset_ratios_dict[select_preset]
